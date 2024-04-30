a1aace8680c785e597bfe4400d0e64eeaa1412fd
kevincruze <139720046+kevincruze@users.noreply.github.com>
2024-04-30 16:27:20 -0700

    Delete HealthcareAttrition directory

--- HealthcareAttrition/JobSatisfactionAttrition.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  JobSatisfaction,
-  COUNT(*) AS TotalEmployees,
-  SUM(CASE WHEN Attrition = TRUE THEN 1 ELSE 0 END) AS AttritionCount,
-  SUM(CASE WHEN Attrition = FALSE THEN 1 ELSE 0 END) AS NonAttritionCount,
-  (SUM(CASE WHEN Attrition = TRUE THEN 1 ELSE 0 END) / Count(*)) * 100 AS PercentAttrition,
-FROM `healthcare-worker-attrition.healthcare_worker_attrition.healthcare_worker_attrition_rate` 
-GROUP BY JobSatisfaction
-ORDER BY JobSatisfaction
--- HealthcareAttrition/SalaryRange.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-  HourlyRate,
-FROM `healthcare-worker-attrition.healthcare_worker_attrition.healthcare_worker_attrition_rate`
-GROUP BY HourlyRate
-ORDER BY HourlyRate DESC
